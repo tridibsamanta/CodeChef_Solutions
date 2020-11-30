58037719a7a1b6ccd385c38bd5896763183b96b8
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-11-30 23:07:45 +0530

    Little Elephant and Candies

---
+++ Practice/LECANDY.cpp
@@ -0,0 +1,42 @@
+/*
+ ~ Author        : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(a, c)           for (int(a) = 0; (a) < (c); (a)++)
+#define w(x)                int x; cin >> x; while(x--)
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int main() {
+
+	FIO;
+
+	w (tc) {
+
+		int n, c;
+		cin >> n >> c;
+
+		int totalCandies = 0;
+
+		for (int i = 0; i < n; ++i) {
+			int curr;
+			cin >> curr;
+			totalCandies += curr;
+		}
+
+		if (totalCandies > c)
+			cout << "No" << '\n';
+		else
+			cout << "Yes" << '\n';
+
+	}
+
+	return 0;
+}
