3ea75ea22c6cf5c01cdd9a013e8e07935496fa52
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-04-13 23:22:41 +0530

    World Record

---
+++ Practice/BOLT.cpp
@@ -0,0 +1,65 @@
+/*
+ ~ Author        : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+void solve() {
+
+	float k1, k2, k3, v;
+	cin >> k1 >> k2 >> k3 >> v;
+
+	float speed = k1 * k2 * k3 * v;
+
+	float time = 100 / speed;
+
+	float ans = (int)(time * 100 + .5);
+	ans = (float)ans / 100;
+
+	float record = 9.58;
+
+	if (ans < record)
+		cout << "YES";
+	else
+		cout << "NO";
+}
+
+int main() {
+
+	FIO;
+
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
